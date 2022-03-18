d301542074693b0c23f9711e552003b30205d5bb
Ciciovan Bogdan <ciciovan.bogdan@gmail.com>
2022-03-18 11:02:48 +0200

    creating the logic

---
+++ src/components/LoginButton.js
@@ -0,0 +1,17 @@
+// rafce
+import React from 'react'
+import { useAuth0 } from '@auth0/auth0-react';
+
+const LoginButton = () => {
+    const { loginWithRedirect, isAuthenticated } = useAuth0();
+
+    return (
+
+            <button onClick={() => loginWithRedirect()}>
+                Login
+            </button>
+
+    )
+}
+
+export default LoginButton
